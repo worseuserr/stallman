c7b87c286d62730e4dde9b25f8df5612abcb80cc
Oskar Wyciszkiewicz <owyciszk@f0r4s11.codam.nl>
2024-11-11 23:24:30 +0100

    adjusted shell scripts

--- push.sh
+++ push.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Define the target directory
 TARGET_DIR="/sgoinfre"
 
-# Check if the target directory exists
 if [ ! -d "$TARGET_DIR" ]; then
   echo "Error: Target directory does not exist."
   exit 1
 fi
 
-# Move all .py files in the current directory to the target directory
-for file in *.py; do
+for file in files/*.py; do
   if [ -f "$file" ]; then
     chmod 755 "$file"
     cp "$file" "$TARGET_DIR"
--- remove.sh
+++ remove.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Specify the hardcoded directory where the Python files are located
 HARD_CODED_DIR="/sgoinfre"
 
-# Check if the directory exists
 if [ ! -d "$HARD_CODED_DIR" ]; then
     echo "The specified directory does not exist: $HARD_CODED_DIR"
     exit 1
 fi
 
-# Iterate through all Python files in the hardcoded directory
 for file in "$HARD_CODED_DIR"/*.py; do
-    # Check if the file exists in the current directory
-    if [ -f "./$(basename "$file")" ]; then
+    if [ -f "./files/$(basename "$file")" ]; then
         echo "Removing Python file: $file"
         rm -rf "$file"
     fi
