f76f6cdaddf13713b628db36d46fb67cf6bb0350
worse <96693239+worseuserr@users.noreply.github.com>
2024-11-15 00:16:01 +0100

    minor adjust

--- sinfo.py
+++ sinfo.py
@@ -2,12 +2,12 @@
 import sys
 import json
 
-# script to easily fetch info about any file in /files as long as there is an entry in the .info.json file.
+# script to easily fetch info about any file in /scripts as long as there is an entry in the .info.json file.
 # use like ./sinfo.py {filename}
 # not case sensitive, shows all entries that match (even just partly) with the filename e.g. "./sinfo.py moul" will show info about MOUL and EMOUL.
 
 if (len(sys.argv) != 2):
-	print("Enter the name (or part thereof) of a filename in /files to get info about the script.\nUsage:\n\t./sinfo.py {filename}\n\t./sinfo.py all")
+	print("Enter the name (or part thereof) of a filename in /scripts to get info about the script.\nUsage:\n\t./sinfo.py {filename}\n\t./sinfo.py all")
 	exit(1)
 
 def output(file, data):
